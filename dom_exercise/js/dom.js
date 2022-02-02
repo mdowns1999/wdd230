0968e5e26397ed62c814a78702f6e97d239056a1
mdowns <michaeljdowns99@gmail.com>
2022-02-02 12:15:50 -0700

    Added More comments

--- dom_exercise/js/dom.js
+++ dom_exercise/js/dom.js
@@ -40,15 +40,20 @@ button.addEventListener('click', function(){
     let input_value = input.value;
     input.value="";
 
+    /*This  will create one list item, button, and area*/
     const list_item = document.createElement('li');
     const list_button = document.createElement('button')
     const list_text = document.createElement('span')
 
-    /*This  will add the user's inputs into list*/
+    /*This  will add the user's inputs as a list item*/
     list_item.appendChild(list_text)
     list_text.textContent = input_value;
+
+    /*This  will add a button on the user's input*/
     list_item.appendChild(list_button)
     list_button.textContent = "❌";
+
+    /*This  will add the user's inputs into list*/
     list.appendChild(list_item);
 
     /*This function will delete the user's item*/
